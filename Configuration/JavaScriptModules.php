2ca1c10c11ce1f29603cc331db6d629c2d630611
David Mellen <d.mellen@remind.de>
2025-02-19 08:33:14 +0100

    feat: add form elements multiselect from form-inspector-editors extension

---
+++ Configuration/JavaScriptModules.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'dependencies' => ['form'],
+    'imports' => [
+        '@remind/form/' => 'EXT:rmnd_form/Resources/Public/JavaScript/',
+    ],
+];
